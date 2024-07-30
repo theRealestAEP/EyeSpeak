66f7ac2a79b53c33863434c8c82f59bc7b10a47b
BuildTools <unconfigured@null.spigotmc.org>
2024-07-30 07:58:01 -0400

    fixed keyboard from moving

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -468,7 +468,7 @@ export default function Home() {
               placeholder="Text will output here..."
               onChange={onChangeInput}
             />
-            <div className="flex">
+            <div className="flex min-h-[80px]">
               <div ref={scrollableDivRef} className="flex flex-wrap mb-2 mt-2 overflow-y-auto" style={{ maxHeight: '10vh' }}>
                 {arrayInput.map((word, index) => {
                   return (
@@ -524,7 +524,7 @@ export default function Home() {
             </div>
             <div className="button-container predict-buttons">
               {/* I need to move these under the buttons below */}
-              {predictionsLoading ? <div role="status">
+              {predictionsLoading ? <div role="status" className="h-[55px]">
                 <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
                   <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
@@ -545,7 +545,7 @@ export default function Home() {
             </div>
           </div>
           <div>
-            <div className="mt-10">
+            <div>
               {rows.map((row, index) => (
                 <div key={index} className="flex justify-center gap-2 mb-2">
                   {row.map((key) => (
@@ -563,7 +563,7 @@ export default function Home() {
           </div>
           <div >
             {/* button to open the modal for saved phrases */}
-            <button className="absolute bottom-0 right-0 bg-purple-600 text-4xl rounded-md p-6 font-bold text-white" onClick={() => setsavedPhrasesOpen(!savedPhrasesOpen)}>Saved Phrases</button>
+            <button className="absolute bottom-0 right-0 bg-purple-600 text-3xl rounded-md p-5 font-bold text-white" onClick={() => setsavedPhrasesOpen(!savedPhrasesOpen)}>Saved Phrases</button>
           </div>
           <div>
             {savedPhrasesOpen && (
